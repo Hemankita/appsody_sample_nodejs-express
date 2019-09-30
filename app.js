b791d2b98830eb239e8e6499d392ab0d85becfcc
Hemankita <Hemankita.Perabathini@ibm.com>
2019-09-30 16:42:22 -0500

    deploy

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hi hema from Appsody! Testing");
+  res.send("Hello hema from Appsody! Testing");
 });
 
 module.exports.app = app;
