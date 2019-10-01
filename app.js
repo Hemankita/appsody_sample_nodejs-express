9ef1d36fcd16b5d3b4cb3470d1543900ad1a9b72
Hemankita <Hemankita.Perabathini@ibm.com>
2019-10-01 11:57:13 -0500

    new namespace test

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello hema from Appsody! Testing");
+  res.send("Welcome to the Kabanero 101");
 });
 
 module.exports.app = app;
