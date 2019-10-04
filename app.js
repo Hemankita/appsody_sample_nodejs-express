48bee75963acf2c4a267272cfa963d6935136999
Hemankita <Hemankita.Perabathini@ibm.com>
2019-10-04 17:24:41 -0500

    nodejs test

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("appsody sample nodejs express");
+  res.send("hi !!! appsody sample nodejs express");
 });
 
 module.exports.app = app;
